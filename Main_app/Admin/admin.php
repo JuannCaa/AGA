2a542cb8d15d6b31df0bcbeaffa7d9c72402e22b
JuannCaa <xst20@hotmail.com>
2020-09-29 08:34:11 -0500

    updated

---
+++ Main_app/Admin/admin.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content= "AGA Refacciones de Monclova">
+    <meta name="author" content="Juan Carlos Garza Jimenez">
+    <meta property="og:image" content="https://raw.githubusercontent.com/JuannCaa/AGA/master/logo%202.0.jpg">
+    <link rel="icon" href="img/Img/icono aga.ico">
+
+    <title>AGA</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
+
+    <!-- Custom CSS -->
+    <link href="css/custom.css" rel="stylesheet">
+    <!-- Font Awesome CSS -->
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    
+   
+</head>
+<!--Hola version 1 2-->
+
+<body data-spy="scroll" data-target="#navbarsExampleDefault">
+
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="#"><img src="img/Img/Logo 3.0.png" class="img-responsive" alt="Styles logo" ></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item  mr-4">
+                        <a class="nav-link" href="#carouselFull">Home </a>
+                    </li>
+                    <li class="nav-item  mr-4">
+                        <a class="nav-link" href="#about">Nosotros</a>
+                    </li>
+                    <li class="nav-item  mr-4">
+                        <a class="nav-link" href="#Marca">Marcas</a>
+                    </li>
+                    <li class="nav-item  mr-4">
+                        <a class="nav-link" href="#catalogo">Catalogos</a>
+                    </li>
+                    <li class="nav-item  mr-4">
+                        <a class="nav-link" href="#contact">Contactanos</a>
+                    </li>
+                    <li class="nav-item  mr-4">
+                        <a href="Login.php" class="btn btn-outline-primary" role="button" aria-pressed="true">Login</a>
+                    </li>
+                </ul>
+            </div>
+        </div>    
+    </nav>
---
+++ Main_app/Usuario/usuario.php
@@ -0,0 +1,339 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content= "AGA Refacciones de Monclova">
+    <meta name="author" content="Juan Carlos Garza Jimenez">
+    <meta property="og:image" content="https://raw.githubusercontent.com/JuannCaa/AGA/master/logo%202.0.jpg">
+    <link rel="icon" href="../../img/Img/icono aga.ico">
+
+    <title>AGA</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
+
+    <!-- Custom CSS -->
+    <link href="../../css/custom.css" rel="stylesheet">
+    <!-- Font Awesome CSS -->
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    
+   
+</head>
+<!--Hola version 1 2-->
+
+<body data-spy="scroll" data-target="#navbarsExampleDefault">
+
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="#"><img src="../../img/Img/Logo 3.0.png" class="img-responsive" alt="Styles logo" ></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item  mr-4">
+                        <a class="nav-link" href="#tienda">Tienda</a>
+                    </li>
+                    <li class="nav-item  mr-4">
+                        <a class="nav-link" href="#servicios">Servicios</a>
+                    </li>
+                    <li class="nav-item  mr-4">
+                        <a class="nav-link" href="#contact">Contactanos</a>
+                    </li>
+                    <li class="nav-item  mr-4">
+                        <a href="../../index.php" class="btn btn-outline-primary" role="button" aria-pressed="true">Logout</a>
+                    </li>
+                </ul>
+            </div>
+        </div>    
+    </nav>
+
+
+    <section id="tienda">
+       <div class="container">
+        <div class="starter-template">
+            <h1 align="center" class="hir">Tienda</h1>
+            <hr class="featurette-divider">
+        </div>
+            
+           <div class="card-group text-center p-1">
+          
+               <div class="row">
+                    <div class="col-md-12 col-lg-4">
+                        <div class="card m-3">
+                            <img class="card-img-top mb-3" src="../../img/tienda/1.jpg" alt="Card image cap">
+                            <div class="card-block">
+                                <h4 class="card-title">Percha para suspension</h4>
+                                <a class="btn btn-info mb-3"   href="https://www.paypal.com/webapps/hermes?token=55543392P93760122&useraction=commit&mfid=1594622190802_cda2ad0f452d6" role="button">Comprar</a>
+                             
+                                
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-12 col-lg-4">
+                        <div class="card m-3">
+                            <img class="card-img-top mb-3" src="../../img/tienda/2.jpg" alt="Card image cap">
+                            <div class="card-block">
+                                <h4 class="card-title">Percha para suspension</h4>
+                                 
+                                <!-- Large modal -->
+                         
+                                <a class="btn btn-info mb-3"   href="https://www.paypal.com/webapps/hermes?token=181499026J175772C&useraction=commit&mfid=1594623204757_e38fa71850287" role="button">Comprar</a>
+                             
+                            
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-12 col-lg-4">
+                        <div class="card m-3">
+                            <img class="card-img-top mb-3" src="../../img/tienda/3.jpg" alt="Card image cap">
+                            <div class="card-block">
+                                <h4 class="card-title">Buje central</h4>
+                                 
+                                <a class="btn btn-info mb-3"   href="https://www.paypal.com/webapps/hermes?token=90N06818RM528425W&useraction=commit&mfid=1594623469861_e7855ec1926b2" role="button">Comprar</a>
+                             
+                                
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-12 col-lg-4">
+                        <div class="card m-3">
+                            <img class="card-img-top mb-3" src="../../img/pdf/4.jpg" alt="Card image cap">
+                            <div class="card-block">
+                                <h4 class="card-title">Cargo Pro Equipment</h4>
+                                <p class="card-text p-2"> Soluciones de alta calidad para sistemas de sujeción y control de carga.</p><br>
+                                <!-- Large modal -->
+                         
+                                <a class="btn btn-info mb-3"   href="Pdf/4.pdf" role="button">Ver</a>
+                             
+                                
+
+                            </div>
+                        </div>
+                    </div>
+                    </section>
+
+<section id=servicios>
+ <!-- START THE FEATURETTES -->
+ <h1  align="center">Servicios</h1>
+ <hr class="featurette-divider">
+ 
+<div class="row featurette">
+  <div class="col-md-7">
+    <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It’ll blow your mind.</span></h2>
+    <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+  </div>
+  <div class="col-md-5">
+    <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+  </div>
+</div>
+
+<hr class="featurette-divider">
+
+<div class="row featurette">
+  <div class="col-md-7 order-md-2">
+    <h2 class="featurette-heading">Oh yeah, it’s that good. <span class="text-muted">See for yourself.</span></h2>
+    <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+  </div>
+  <div class="col-md-5 order-md-1">
+    <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+  </div>
+</div>
+
+<hr class="featurette-divider">
+
+<div class="row featurette">
+  <div class="col-md-7">
+    <h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
+    <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+  </div>
+  <div class="col-md-5">
+    <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+  </div>
+</div>
+
+<hr class="featurette-divider">
+
+<!-- /END THE FEATURETTES -->
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <section id="hire">
+        <div class="container-fluid">
+            <div class="jumbotron jumbotron-fluid">
+               <div class="container hir">
+                   <marquee direction="left"><h1 class="display-3"><img src="../../img/Img/Mobile_phone_rings.gif" alt="chef"> Contactanos ahora al 8666417088 </h1></marquee>         
+               </div>
+           </div>
+        </div>
+    </section>
+
+    <section id="contact">
+        <div class="container">
+            <div class="text-center mb-5">
+                <h2>Contacta con nosotros hoy</h2>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-6 pb-5">
+                    <h2>Contactanos</h2>
+                    <p> Rellene el formulario y le contestaremos lo antes posible o tambien puede contactarnos por telefono al :  </p>
+                    
+                    <h2>8666417088</h2>
+                   
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-6">
+                    <form action="contact.php" method="post" enctype="multipart/form-data">
+                       <div class="form-group">
+                           <label for="names"><h2>Nombre</h2></label>
+                           <input name="nombre" type="text" class="form-control" id="names" placeholder="Juan" required>
+                       </div>
+                       <div class="form-group">
+                           <label for="emails"><h2>Email</h2></label>
+                           <input name="correo" type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email" required>
+                       </div>
+                       <div class="form-group">
+                           <label for="messages"><h2>Comentario</h2></label>
+                           <textarea name="comentario" class="form-control" id="messages" placeholder="hola mi numero es,estoy interesado,me gustaria saber precios." rows="3"></textarea>
+                       </div>
+                       <button type="submit" class="btn btn-info btn-lg">Enviar</button>
+                   </form>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    <section>
+        <div class="map">
+            <iframe src= "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14237.45294230791!2d-101.429413!3d26.8601983!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb149be0f8d02e754!2sAGA%20REFACCIONES%20Y%20LUBRICANTES!5e0!3m2!1ses-419!2smx!4v1589417509601!5m2!1ses-419!2smx" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+        </div>
+    </section>
+    
+    <section id="footer">
+        <div class="container">
+            <div class="row">
+                <div class="blogo col-xs-12 col-sm-12 col-md-12 col-lg-4">
+                    <a href="#"><img src="../../img/Img/Logo 3.0.png" alt="Styles logo"></a>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-4">
+                    <div class="cpt text-light text-center">
+                        <p>© 2019-Aga Refacciones.</p>                    
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-4">
+                    <div class="ftaw text-light text-center">
+                        <a href="https://www.facebook.com/AGAREFACCIONES/" target="_blank"><i class="fa fa-facebook-square"></i></a> 
+                        <a href="https://twitter.com/22ITSolutions1" target="_blank"><i class="fa fa-twitter-square"></i></a> 
+                       
+                        <a href="https://www.instagram.com/?hl=en" target="_blank"><i class="fa fa-instagram"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    <div id="bttp" class="btt">
+        <a href="#"><i class="fa fa-arrow-circle-up"></i></a>
+    </div>
+    
+    
+
+    
+    
+    
+   
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>     
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+   
+    <!-- Scrollspy -->
+    <script>$('body').scrollspy({ target: '#navbarsExampleDefault', offset: 108 })</script>
+    <!-- Smooth Scroll -->
+    <script src="../../js/smooth-scroll.js"></script>
+    <!-- Video Full Width -->
+    <script src="../../js/jquery.vide.js"></script> 
+    <script>
+        var scroll = new SmoothScroll('a[href*="#"]');
+    </script>
+    <!-- Number Counter -->
+    <script src="../../js/nsc.js"></script>
+    <!-- Video -->
+    <script async src="https://www.youtube.com/iframe_api"></script>
+    <script>
+     function onYouTubeIframeAPIReady() {
+      var player;
+      player = new YT.Player('muteYouTubeVideoPlayer', {
+        videoId: 'XTMRHrpnSTY', // YouTube Video ID
+        width: 560,               // Player width (in px)
+        height: 316,              // Player height (in px)
+        playerVars: {
+          autoplay: 1,        // Auto-play the video on load
+          controls: 1,        // Show pause/play buttons in player
+          showinfo: 0,        // Hide the video title
+          modestbranding: 1,  // Hide the Youtube Logo
+          loop: 1,            // Run the video in a loop
+          fs: 0,              // Hide the full screen button
+          cc_load_policy: 0, // Hide closed captions
+          iv_load_policy: 3,  // Hide the Video Annotations
+          autohide: 0         // Hide video controls when playing
+        },
+        events: {
+          onReady: function(e) {
+            e.target.mute();
+          }
+        }
+      });
+     }
+
+     // Written by @labnol 
+    </script>
+    <script>
+        myID = document.getElementById("bttp");
+
+        var myScrollFunc = function() {
+          var y = window.scrollY;
+          if (y >= 1200) {
+            myID.className = "btt show"
+          } else {
+            myID.className = "btt hide"
+          }
+        };
+
+        window.addEventListener("scroll", myScrollFunc);
+    </script>
+</body>
+</html>
